e2b8f07f3f1f2c3ab4047fb4dfa10335a3f93abd
Vlad <vlad.miryaha@gmail.com>
2018-10-08 00:32:48 +0300

    fixit

--- hw.c
+++ hw.c
@@ -5,12 +5,14 @@
 #include<sys/wait.h>
 #include<sys/types.h>
 #include<assert.h>
+
+// FIXIT: нужно переименовать Max_size и Max_Time в MAX_SIZE, MAX_TIME
+
 #define Max_size 100
 #define Max_Time 5
 
-
-void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
-
+void Split(char* string,char* delimiters,char*** tokens,int* tokensCount)
+{
 	int len = strlen(string);
 	(*tokensCount) = 0;
 	(*tokens)[*tokensCount] = strtok(string, delimiters);
@@ -21,7 +23,8 @@ void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
 	}
 }
 
-int main(){
+int main()
+{
 	int i;
 	char string[Max_size] = {};
 	int tokensCount = 0;
@@ -31,6 +34,9 @@ int main(){
 	char delimiters[2] = {'\t','\n'};
 	FILE *mf;
 	mf = fopen("example.txt", "r");
+	
+	// FIXIT: 1) название estr не очень понятное, нужно переименовать
+	// 2) можно же написать do {} while (estr);
 	do {
 		estr = fgets (str,Max_size,mf);
 		strcat(string, str);
@@ -42,13 +48,15 @@ int main(){
 	char **tokens;
 	tokens = (char**)malloc(Max_size * sizeof(char*));
 
-	for(i = 0; i < Max_size; i++){
+	for(i = 0; i < Max_size; i++) {
 		tokens[i] = (char*)malloc(Max_size * sizeof(char));
 	}
 
 	Split (string, delimiters, &tokens, &tokensCount);
-	for (i = 0; i < tokensCount; i++){
+	for (i = 0; i < tokensCount; i++) {
+		// FIXIT: переименуйте, пожалуйста, agrv_ в argv
 		char* argv_[Max_size] = {0};
+		// FIXIT: следующие 5 строк дублируют код ф-и split
 		int j = 0;
 		argv_[0] = strtok(tokens[i], " ");
 		do {
@@ -59,11 +67,14 @@ int main(){
 		pid_t pid;
 		pid = fork();
 
-		if (pid == 0){
+		if (pid == 0) {
 			pid = fork();
 
-			if (pid == 0){
+			if (pid == 0) {
 				sleep(atoi(argv_[0]));
+				// FIXIT: вместо этого цикла со сдвигом на 1 можно же просто написать
+				// argv++;
+				// execvp(argv[0], argv);
 				int g;
 
 				for(g = 0; g < Max_size - 1; g++){
@@ -78,16 +89,13 @@ int main(){
 				if (waitpid(pid, &status, WNOHANG))
 					exit(0);
 				else {
-
 					kill(pid, SIGKILL);
 					exit(0);
 				}
 			}
 		}
 	}
-
-	free(tokens);
-	return 0;
 	
+	free(tokens);
+	return 0;	
 }
-
