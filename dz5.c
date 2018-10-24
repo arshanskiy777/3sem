798a4cf7aa1aefe87703df596a0211e878d25ff0
Vlad <vlad.miryaha@gmail.com>
2018-10-24 22:13:48 +0300

    fixit

--- dz5.c
+++ dz5.c
@@ -8,26 +8,19 @@
 
 #define SIZE 1000
 
-void Write_Read (int procIndec, int *fd, char *input, char *output) {
-
-        if (procIndec == 1) {
-
+void Write_Read (int procIndex, int *fd, char *input, char *output) {
+        if (procIndex == 1) {
                 fgets(input, SIZE, stdin);
                 write(fd[1], input, SIZE);
-
-        } else if (procIndec == 0) {
-
+        } else if (procIndex == 0) {
                 read(fd[0], output, SIZE);
                 fprintf(stdout, "%s", output);
-
         }
-
 }
 
 
 int main (int argc, char *argv[]) {
-
-
+        // Address
         char name1[] = "aaa1fifo", name2[] = "aaa2fifo", *fileAdressRead, *fileAdressWrite;
         char input[SIZE] = {0};
         char output[SIZE] = {0};
@@ -44,9 +37,7 @@ int main (int argc, char *argv[]) {
                 fileAdressWrite = name1;
                 fileAdressRead = name2;
                 printf ("Write: %s      Read: %s       Zero client\n\n", fileAdressWrite, fileAdressRead);
-
         } else {
-
                 fileAdressWrite = name2;
                 fileAdressRead = name1;
                 printf ("Write: %s       Read: %s      First client \n\n", fileAdressWrite, fileAdressRead);
@@ -54,11 +45,44 @@ int main (int argc, char *argv[]) {
 
         pid_1 = fork();
 
+        // Index
         int procIndec;
 
         if (clientID == 0) { // O clientID
 
                 if (pid_1 == 0) {
+                        
+                        // FIXIT: у вас дублируются куски кода
+                        
+                        /*
+                        procIndec = 1;
+                        fd[1] = open (fileAdressWrite, O_WRONLY);
+                        while(1) {
+                                Write_Read (procIndec, fd, input, output);
+                        }
+                        */
+                        
+                        /*
+                        procIndec = 0;
+                        fd[0] = open (fileAdressRead, O_RDONLY);
+                        while(1) {
+                                Write_Read (procIndec, fd, input, output);
+                        }
+                        */
+                        
+                        // Причем других вариантов использования нет.
+                        // Кажется что можно тогда удалить WriteRead ф-ю, а создать две
+                        // startReader, startWriter
+                        // например в startWriter будет примерно такой код
+                        /*
+                        fd[1] = open (fileAdressWrite, O_WRONLY);
+                        while(1) {
+                                fgets(input, SIZE, stdin);
+                                write(fd[1], input, SIZE);
+                        }
+                        В итоге дублироваться код не будет
+                        */
+                        
                         procIndec = 1;
                         fd[1] = open (fileAdressWrite, O_WRONLY);
                         while(1) {
