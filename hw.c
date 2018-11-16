d7eacd28e8259b0416ee7b5f2252019cbcc6f438
arshanskiy777 <37326743+arshanskiy777@users.noreply.github.com>
2018-11-16 19:04:23 +0300

    Update hw.c

--- hw.c
+++ hw.c
@@ -6,96 +6,75 @@
 #include<sys/types.h>
 #include<assert.h>
 
-// FIXIT: нужно переименовать Max_size и Max_Time в MAX_SIZE, MAX_TIME
+#define MAX_SIZE 100
+#define MAX_TIME 5
 
-#define Max_size 100
-#define Max_Time 5
+void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
 
-void Split(char* string,char* delimiters,char*** tokens,int* tokensCount)
-{
 	int len = strlen(string);
-	(*tokensCount) = 0;
+	*tokensCount = 0;
 	(*tokens)[*tokensCount] = strtok(string, delimiters);
-
 	while((*tokens)[*tokensCount] != NULL){
- 		(*tokensCount)++;
- 		(*tokens)[*tokensCount] = strtok(NULL, delimiters);
+		(*tokensCount)++;
+		(*tokens)[*tokensCount] = strtok(NULL, delimiters);
 	}
 }
 
-int main()
-{
+int main(){
 	int i;
-	char string[Max_size] = {};
+	char string[MAX_SIZE] = {};
 	int tokensCount = 0;
 	char *estr;
 	int status;
-	char str[Max_size] = {};
+	char str[MAX_SIZE] = {};
 	char delimiters[2] = {'\t','\n'};
 	FILE *mf;
-	mf = fopen("example.txt", "r");
-	
-	// FIXIT: 1) название estr не очень понятное, нужно переименовать
-	// 2) можно же написать do {} while (estr);
-	do {
-		estr = fgets (str,Max_size,mf);
+	mf = fopen("primer.txt", "r");
+	do{
+      		estr = fgets(str, MAX_SIZE, mf);
 		strcat(string, str);
 		if (estr == NULL){
 			break;
 		}
-	} while (1);
-
+	}while(estr);
+	
 	char **tokens;
-	tokens = (char**)malloc(Max_size * sizeof(char*));
-
-	for(i = 0; i < Max_size; i++) {
-		tokens[i] = (char*)malloc(Max_size * sizeof(char));
+	tokens = (char**)calloc(MAX_SIZE, sizeof(char*));
+	for(i = 0; i < MAX_SIZE; i++){
+		tokens[i] = (char*)calloc(MAX_SIZE, sizeof(char));
 	}
-
-	Split (string, delimiters, &tokens, &tokensCount);
-	for (i = 0; i < tokensCount; i++) {
-		// FIXIT: переименуйте, пожалуйста, agrv_ в argv
-		char* argv_[Max_size] = {0};
-		// FIXIT: следующие 5 строк дублируют код ф-и split
+	Split(string, delimiters, &tokens, &tokensCount);
+	char **argv;
+	argv = (char**)calloc(MAX_SIZE, sizeof(char*));
+	int k;
+	for(k = 0; k < MAX_SIZE; k++){
+		argv[k] = (char*)calloc(MAX_SIZE, sizeof(char));
+	}
+	for (i = 0; i < tokensCount; i++){
 		int j = 0;
-		argv_[0] = strtok(tokens[i], " ");
-		do {
-			j++;
-			argv_[j] = strtok(NULL, " ");
-		} while (argv_[j] != NULL);
-		
-		pid_t pid;
+		char* delimiter = " ";
+		Split(tokens[i], delimiter, &argv, &j);
+		int timeout = atoi(argv[0]);
+		pid_t  pid;
 		pid = fork();
-
-		if (pid == 0) {
+		if (pid == 0){
 			pid = fork();
-
-			if (pid == 0) {
-				sleep(atoi(argv_[0]));
-				// FIXIT: вместо этого цикла со сдвигом на 1 можно же просто написать
-				// argv++;
-				// execvp(argv[0], argv);
-				int g;
-
-				for(g = 0; g < Max_size - 1; g++){
-
-					argv_[g] = argv_[g + 1]; 
-				}
-
-				execvp(argv_[0], argv_);
-			
-			} else {
-				sleep(atoi(argv_[0]) + Max_Time);
-				if (waitpid(pid, &status, WNOHANG))
+			if (pid == 0){
+				sleep(timeout);
+				execvp(argv[1], argv + 1);
+				exit(0);
+			} else { 
+				sleep(timeout + MAX_TIME);
+				if(waitpid(pid, &status, WNOHANG))
 					exit(0);
-				else {
+				else
+				{
 					kill(pid, SIGKILL);
 					exit(0);
 				}
 			}
 		}
 	}
-	
-	free(tokens);
-	return 0;	
+        free(tokens);
+        return 0;	
 }
